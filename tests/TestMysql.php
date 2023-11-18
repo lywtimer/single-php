7159ae17081c3715f50370ab165804a0ee9ffa19
lywtimer <lywtimer@outlook.com>
2023-11-19 00:33:26 +0800

    test response

--- tests/TestMysql.php
+++ tests/TestMysql.php
@@ -2,6 +2,7 @@
 
 namespace mszl\lib;
 
+use mszl\lib\types\UserProfile;
 use mszl\lib\utils\Api;
 use PHPUnit\Framework\TestCase;
 
@@ -9,7 +10,7 @@ class TestMysql extends TestCase
 {
     public function testConnect() {
 
-        var_dump(Api::r('CourseService'));
+        var_dump(Api::r(new UserProfile()));
         $this->assertIsBool(true);
     }
 }
